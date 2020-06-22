96b4dc9f76bc4bd3458b30fe20bf8deec195f257
chedid <yagorogerio020300@gmail.com>
2020-06-22 20:46:07 -0300

    criacao da tela de relatorio com grafico basico

---
+++ gameup/Pages/Gerente/Relatorios.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageGerente.master" AutoEventWireup="true" CodeFile="Relatorios.aspx.cs" Inherits="Pages_Gerente_Relatorios" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', { 'packages': ['corechart'] });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable(<%=ObterDadosDoUsuario()%>);
+
+
+            var options = {
+                title: 'Company Performance',
+                curveType: 'function',
+                legend: { position: 'bottom' }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+
+            chart.draw(data, options);
+        }
+    </script>
+    <div class="card shadow mb-7 col-8">
+        <!-- Card Header - Accordion -->
+        <a href="#collapseCardExample5" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+            <h6 class="m-0 font-weight-bold text-center">GRAFIFICO SEMESTRAL DE DESEMPENHO DO SETOR</h6>
+        </a>
+        <!-- Card Content - Collapse -->
+        <div class="collapse show" id="collapseCardExample5">
+
+            <div id="curve_chart" style="min-height: 400px;"></div>
+
+        </div>
+        
+    </div>
+    <div class="card shadow mb-3 col-4"></div>
+</asp:Content>
+
---
+++ gameup/Pages/Gerente/Relatorios.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pages_Gerente_Relatorios : System.Web.UI.Page
+{
+    private static Usuario usuarioLogado;
+    private static MissaoUsuario missUsuarioLogado;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        validarSessao();
+
+        if (!IsPostBack)
+        {
+            
+        }
+    }
+
+    public string ObterDadosDoUsuario()
+    {
+        DataTable dados = new DataTable();
+
+        //coluna dos dados
+        dados.Columns.Add(new DataColumn("Data", typeof(string)));
+        dados.Columns.Add(new DataColumn("Missões Concluidas", typeof(string)));
+        dados.Columns.Add(new DataColumn("Pontos", typeof(string)));
+        dados.Columns.Add(new DataColumn("Experiencia", typeof(string)));
+        // Os dados que serão mostrados no charts
+
+        dados.Rows.Add(new object[] { 2016, 10, 24, 44 });
+        dados.Rows.Add(new object[] { 2017, 2, 44, 33  });
+        dados.Rows.Add(new object[] { 2018, 3, 7, 11 });
+        dados.Rows.Add(new object[] { 2016, 10, 24, 44 });
+        dados.Rows.Add(new object[] { 2017, 2, 44, 33 });
+        dados.Rows.Add(new object[] { 2018, 3, 7, 11 });
+
+        string strDados;
+
+        strDados = "[['Data','Missões Concluidas', 'Pontos', 'Experiencia'],";
+
+        foreach (DataRow dr in dados.Rows)
+        {
+            strDados = strDados + "[";
+            strDados = strDados + "'" + dr[0] + "'" + "," + dr[1] + "," + dr[2] + "," + dr[3];
+            strDados = strDados + "],";
+        }
+        strDados = strDados + "]";
+
+
+        return strDados;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void validarSessao()
+    {
+        if (Session["USUARIO"] == null)
+        {
+            Response.Redirect("../Visitante/Login.aspx");
+        }
+        else
+        {
+            usuarioLogado = (Usuario)Session["USUARIO"];
+
+            if (usuarioLogado.Tus_id != 1 && usuarioLogado.Tus_id != 2) // Colaborador ou Gerente
+            {
+                Response.Redirect("../Visitante/Login.aspx");
+            }
+        }
+    }
+
+}
